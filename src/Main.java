980806c1c74754d6260d8c915d2ad4d68505ba25
Николай <nike291@yandex.ru>
2022-06-04 08:34:56 +0500

    Домашка по строкам

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        //Задача 1 - Сложение строк
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+        System.out.println(" ");
+
+        //Задача 2 - Перевод в верхний регистр
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        System.out.println(" ");
+
+        //Задача 3 - Замена символа ё на е
+        fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName.replace('ё', 'е'));
+
+
     }
 }
